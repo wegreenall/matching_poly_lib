23ffcca060155ff246b0f4cc83432b754d2bc250
wegreenall <wjgreenall@gmail.com>
2023-05-25 09:20:35 +0300

    added rustfmtformatting and tests for fully connected graphs

--- src/lib.rs
+++ src/lib.rs
@@ -1,52 +1,85 @@
 pub mod matching;
+pub mod matching_raw_memory;
 
-use matching::{get_matching_polies_stable_graph, get_deck, Graph, _calculate_matching_polynomial_binary};
-use matching::{calculate_matching_polynomial_static};
+use matching::calculate_matching_polynomial_static;
+use matching::{
+    get_deck, get_matching_polies_stable_graph, Graph, _calculate_matching_polynomial_binary,
+};
 
-
-
+use matching_raw_memory::{calculate_matching_polynomial_raw, GraphData, GraphProperties};
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn check_matching_polynomial_static_2() {
-        let data = [83397, 39080, 19209, 8859, 4503, 3121, 1130,
-                    734, 364, 147, 103, 41, 28, 8, 6, 3, 1,
-                     0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
-                     0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
-                     0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
-                     0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
-                     0,  0, 0, 0, 0, 0, 0];
-        let graph = Graph::new(data);// the fully connected graph
+    fn static_polynomial_calculation_hard() {
+        let data = [
+            83397, 39080, 19209, 8859, 4503, 3121, 1130, 734, 364, 147, 103, 41, 28, 8, 6, 3, 1, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        ];
+        let graph = Graph::from(data); // the fully connected graph
         let graph_size = graph.graph_size();
-        let matching_poly = calculate_matching_polynomial_static(graph); 
+        let matching_poly = calculate_matching_polynomial_static(graph);
         //println!("matching poly: {:?}", matching_poly);
-        let graph2 = Graph::new(data);// the fully connected graph
+        let graph2 = Graph::from(data); // the fully connected graph
         let matching_poly_2 = _calculate_matching_polynomial_binary(graph2);
 
         //println!("matching poly 2: {:?}", matching_poly_2);
         assert_eq!(&matching_poly[..=graph_size], matching_poly_2.data());
-        
     }
 
     #[test]
-    fn check_matching_polynomial_static() {
-        let data = [512, 256, 128, 64, 32, 16, 8, 4, 2, 1,
-        0, 0, 0, 0,0, 0,0, 0,0, 0, 0,   0,  0, 0,0, 0,0, 0, 0, 0,0, 0, 0, 0,
-        0, 0, 0, 0, 0,0, 0,0, 0,0, 0, 0, 0,0, 0,0, 0,0, 0, 0, 0,0, 0, 0, 0, 0, 0, 0, 0,0];
-        let graph = Graph::new(data);// the fully connected graph
+    fn static_polynomial_calculation_fully_connected() {
+        let data = [
+            512, 256, 128, 64, 32, 16, 8, 4, 2, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0,
+        ];
+        let graph = Graph::from(data); // the fully connected graph
         let graph_size = graph.graph_size();
-        let matching_poly = calculate_matching_polynomial_static(graph); 
-        //println!("matching poly: {:?}", matching_poly);
-        let graph2 = Graph::new(data);// the fully connected graph
+        let matching_poly = calculate_matching_polynomial_static(graph);
+
+        let graph2 = Graph::from(data); // the fully connected graph
+        let matching_poly_2 = _calculate_matching_polynomial_binary(graph2);
+
+        assert_eq!(&matching_poly[..=graph_size], matching_poly_2.data());
+    }
+
+    #[test]
+    fn raw_polynomial_calculation_hard() {
+        let data = [
+            83397, 39080, 19209, 8859, 4503, 3121, 1130, 734, 364, 147, 103, 41, 28, 8, 6, 3, 1, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        ];
+        let graph = data; // the fully connected graph
+        let graph_size = graph.graph_size();
+        let matching_poly = calculate_matching_polynomial_raw(graph);
+
+        let graph2 = Graph::from(data); // the fully connected graph
         let matching_poly_2 = _calculate_matching_polynomial_binary(graph2);
 
         //println!("matching poly 2: {:?}", matching_poly_2);
         assert_eq!(&matching_poly[..=graph_size], matching_poly_2.data());
     }
+    #[test]
+    fn raw_polynomial_calculation_fully_connected() {
+        let data = [
+            512, 256, 128, 64, 32, 16, 8, 4, 2, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0,
+        ];
 
+        let graph = data; // the fully connected graph
+        let graph_size = graph.graph_size();
+        let matching_poly = calculate_matching_polynomial_raw(graph);
+
+        let graph2 = Graph::from(data); // the fully connected graph
+        let matching_poly_2 = _calculate_matching_polynomial_binary(graph2);
+
+        //println!("matching poly 2: {:?}", matching_poly_2);
+        assert_eq!(&matching_poly[..=graph_size], matching_poly_2.data());
+    }
 }
-
-
