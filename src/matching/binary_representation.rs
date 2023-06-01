43d0f6856de803d4fe9961b15d3f17241c50142f
wegreenall <wjgreenall@gmail.com>
2023-06-01 12:28:14 +0300

    save for travel to England

--- src/matching/binary_representation.rs
+++ src/matching/binary_representation.rs
@@ -237,35 +237,3 @@ pub fn _calculate_matching_polynomial_binary(graph: Graph) -> Polynomial<u64> {
         return poly
     }
 } 
-
-/// The weighted matching polynomial is defined via the edge-deletion recurrence:
-/// Q(G, x) = Q(G - e, x) + w(e)^2 * Q(G - N(e), x) 
-/// where e is an edge in G, and N(e) is the pair of nodes connected to e,
-/// and w(e) is the weight associated with e. 
-pub fn _calculate_weighted_matching_polynomial_binary(graph: Graph) -> Polynomial<u64> {
-    // the base case for the process is that the graph is edgeless.
-    // This means that, of the remaining nodes, each of their integer
-    // representations is a power of two.
-    if graph.edgeless() { // i.e. we're at the base case.
-        // produce a sequence of coefficients the same length as the number of vertices
-        //println!("Hit edgeless graph! with {} nodes", graph.edgeless_node_count());
-        let mut coeffics = vec![0; graph.edgeless_node_count()];
-        coeffics.push(1);
-        let poly = Polynomial::new(coeffics);
-        //println!("Polynomial: {:?}", poly);
-        //println!("graph {:?}", graph.data);
-        return poly
-    } else {
-        // get G' and G''
-        // G' = G - an edge
-        // G'' = G - the nodes connected to the edge removed to get G'
-        let (graph_prime, graph_prime_prime) = graph.get_graph_primes();
-
-
-
-        let poly_1 = _calculate_matching_polynomial_binary(graph_prime);
-        let poly_2 = _calculate_matching_polynomial_binary(graph_prime_prime);
-        let poly = poly_1 + squared_weight * poly_2;
-        return poly
-    }
-} 
--- src/matching/mod.rs
+++ src/matching/mod.rs
@@ -3,6 +3,6 @@ mod graphs;
 mod petgraph;
 mod matching_poly;
 
-pub use self::graphs::{_calculate_matching_polynomial_binary, get_deck, Graph};
+pub use self::graphs::{_calculate_matching_polynomial_binary,_calculate_weighted_matching_polynomial_binary, get_deck, Graph};
 pub use self::petgraph::{_calculate_matching_polynomial, get_matching_polies_stable_graph};
 pub use self::matching_poly::calculate_matching_polynomial_pointer;
