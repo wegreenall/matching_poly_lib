9e73b26f4e6443bd8c0d4c2fc61a8747f2930b99
wegreenall <wjgreenall@gmail.com>
2023-10-09 10:33:43 +0300

    Added weighted polynomial calculation functions and tests

--- src/weighted_graph_matching/weighted_polynomial_calculation.rs
+++ src/weighted_graph_matching/weighted_polynomial_calculation.rs
@@ -1,13 +1,61 @@
 use std::mem::size_of;
-use itertools::iproduct;
+use itertools::{iproduct, izip};
 use itertools::Itertools;
 use permutator::{Permutation, KPermutationIterator, k_permutation};
+use std::collections::HashSet;
+use std::iter;
+use crate::{BinaryGraph, calculate_matching_polynomial_pointer_addresses};
+use crate::traits::Graph;
+
 
 
 const MAX_NODES: usize = size_of::<usize>()* 8;
 const NUM_SIZE: usize = size_of::<usize>() * 8;
+const POLY_SIZE: usize = size_of::<usize>() * 8;
+
+/// The following two functions together calculate the weighted matching polynomial by
+/// building the address and then in situ calculating the weight.
+pub fn weighted_matching_polynomial_addresses(graph: BinaryGraph, weights: &[f32; MAX_NODES * MAX_NODES]) -> [f32; size_of::<usize>()*8] {
+        let poly: &mut [f32; POLY_SIZE] = &mut [0.0; POLY_SIZE];
+        let address = 1;
+
+        for i in 0..POLY_SIZE {
+            poly[i] = 0.0;
+        }
+        _calculate_weighted_matching_polynomial_static_addresses(graph, weights, poly, address);
+        *poly
+}
+
+fn _calculate_weighted_matching_polynomial_static_addresses<T: Graph>(graph: T, weights: &[f32; MAX_NODES * MAX_NODES], poly: &mut [f32; POLY_SIZE], current_address: usize) {
+    if graph.edgeless() {
+        let node_count = graph.edgeless_node_count();
+        poly[node_count] += weight_from_address(current_address, weights, graph.initial_graph_size());
+    } else {
+        let (graph_prime, graph_prime_prime) = graph.get_graph_primes();
+        _calculate_weighted_matching_polynomial_static_addresses(graph_prime_prime, weights, poly, current_address << 1);
+        _calculate_weighted_matching_polynomial_static_addresses(graph_prime, weights, poly, (current_address << 1) + 1); // put this at the end as I think
+    }
+}
+
+
+/// The following function will accept a sequence of addresses, and weights,
+/// and then build the weighted matching polynomial from them.
+pub fn weighted_matching_polynomial_from_addresses(addresses: Vec<usize>, weights: &[f32; MAX_NODES * MAX_NODES], graph_size: usize) -> [f32; size_of::<usize>()*8] {
+    let poly: &mut [f32; POLY_SIZE] = &mut [0.0; POLY_SIZE];
+    for i in 0..POLY_SIZE {
+        poly[i] = 0.0;
+    }
+    addresses.iter().for_each(|address| {
+        // get the relevant coefficient
+        let coefficient = graph_size - 2 * (address.count_zeros() - address.leading_zeros()) as usize;
+        poly[coefficient] += weight_from_address(*address, weights, graph_size);
+    });
+    *poly
+}
+
 /// Given an address, and a set of weights, 
 /// calculates the relevant weight for the path that is implied by the address.
+/// graph_size should be the current graph_size
 pub fn weight_from_address(address: usize, weights: &[f32; MAX_NODES * MAX_NODES], graph_size: usize) -> f32 {
     // start with a 1-weight
     let mut weight = 1.0;
@@ -60,7 +108,7 @@ pub fn weight_from_address(address: usize, weights: &[f32; MAX_NODES * MAX_NODES
 /// return the calculated. 
 ///
 /// 
-pub fn get_next_weight(//address: usize,
+fn get_next_weight(//address: usize,
                        weights: &[f32; MAX_NODES * MAX_NODES],
                        //address_len: usize,
                        //mask: usize,
@@ -104,56 +152,154 @@ pub fn get_next_weight(//address: usize,
     weights[index]
 }
 
-pub fn weighted_coefficient_calculation(poly: [u64; size_of::<usize>()*8], weights: &[f32; MAX_NODES * MAX_NODES], graph_size: usize, coeffic: usize) -> f64 {
+/// THis calculates the weighted matching polynomial from a matrix of weights,
+/// via the method of calculating the sum of products of the weights.
+/// This is the function endpoint for the permutation-based method described below. Too slow!
+pub fn weighted_polynomial_calculation(weights: &[f32; MAX_NODES * MAX_NODES], graph_size: usize) -> [f32; POLY_SIZE] {
+    let mut poly = [0.0; POLY_SIZE];
+    let range = (0..=graph_size).rev();
+    for (i, coeff_index) in range.enumerate() {
+        // coefficient
+        let coefficient = weighted_coefficient_calculation(weights, graph_size, coeff_index);
+
+        // add to the polynomial
+        poly[i] = coefficient;
+    }
+    poly 
+}
+
+/// This uses a permutation-based mechanism which is too slow!
+pub fn weighted_coefficient_calculation(weights: &[f32; MAX_NODES * MAX_NODES], graph_size: usize, coeffic: usize) -> f32 {
     // giben the polynomial poly, and the weights, calculate the weighted 
     // coefficient as marked by coeffic
-    let mut weighted_coefficient = 1.0;    
-
-    // can probably just pass the value
-    let missing_nodes = graph_size - 2 * coeffic;
-
-    // get all the combinations of these missing node numbers
-    let mut non_zero_weights = weights
+    if coeffic == 0 {
+        return 1.0
+    }
+    // the following iterator is now an iterator over the indices of non-zero weights.
+    let non_zero_weight_indices = weights
         .iter()
         .enumerate()
         .filter(|(_, x)| **x != 0.0) // filter to positive
         .filter(|(i, _)| {  // filter to upper diagonal
             let res = (i / graph_size, i % graph_size);
             res.0 <= res.1
+         })
+        .map(|(i, _)| i)
+        .collect::<Vec<usize>>();
+
+    // We want to produce an iterator whose element is a tuple/iterator of 
+    // indices. The product of each of the indices is one of the weights;
+    // the sum of these products is the coefficient
+    let my_iterators = iter::repeat(non_zero_weight_indices.clone())
+        .take(coeffic/2)
+        .multi_cartesian_product()
+        .map(|v| {
+            let mut v = v;
+            v.sort();
+            v
         })
-        .map(|(i, x)| (i, *x))
-        .collect::<Vec<(usize, f32)>>();
+        .unique();
 
-    for stuff in non_zero_weights.clone() {
-        println!("{:?}", stuff);
+
+    // for each of the k index sets, keep the pairs such that the column and row of each of the elements is not shared by
+    // any of the other indices.
+    let full_hashsets = my_iterators
+        .clone()
+        .map(|v| // looks like 1, 3, 11
+                {
+                 let hashsets = v
+                  .iter() // turn into an iterator
+                  //.map(|x| {
+                      //println!("x: {}", x);
+                      //x
+                  //})
+                  .map(|i| HashSet::from([i / graph_size, i % graph_size])) // convert to hashsets of the column and row
+                  .fold(HashSet::new(), |acc, hs| {
+                      acc
+                          .union(&hs)
+                          .cloned()
+                          .collect()
+                  }); // fold into a single hashset
+                     hashsets
+                });
+    let iterator = my_iterators.zip(full_hashsets)
+                         .filter(|(_, hs)| hs.len() == coeffic) // filter out the hashsets that have a length of 0;
+                         .map(|(set, _)| set); 
+
+    // now get the coefficient using this big iterator
+    let coeffic = iterator
+        .map(|v| 
+             {
+                v.iter()
+                .map(|i| weights[*i])
+                .product::<f32>()
+             })
+        .sum::<f32>();
+    coeffic
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test_weighted_coefficient_calculation() {
+        let true_weights: [f32; 16] = [0.0, 2.0, 0.0, 2.0, 
+                                       2.0, 0.0, 2.0, 0.0,
+                                       0.0, 2.0, 0.0, 2.0, 
+                                       2.0, 0.0, 2.0, 0.0];
+        let true_poly = [3, 0, 6, 0, 1];
+
+        //let poly = Polynomial::new(vec![8.0, 0.0, 8.0, 0.0, 1.0]);
+        let mut poly = [0 as u64; size_of::<u64>() * 8];
+        let mut weights: [f32; 4096] = [0.0; 4096];
+        weights[..16].copy_from_slice(&true_weights);
+        poly[..5].copy_from_slice(&true_poly);
+        let weighted_coefficient = weighted_coefficient_calculation(&weights, 4, 2);
+        assert_eq!(weighted_coefficient, 8.0);
     }
-    //let permutator = HeapPermutationIterator::new(&mut non_zero_weights);
-    let mut counter = 0;
-    let mut perms = Vec::<Vec<&(usize, f32)>>::new();
 
-    let k_permutation_iterator = KPermutationIterator::new(&mut non_zero_weights, coeffic);
-    for perm in k_permutation_iterator {
-        println!("{:?}", perm);
+    #[test]
+    fn test_weighted_polynomial_static_addresses() {
+        let data = [
+            0b1101, 0b110, 0b11, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0,
+        ];
+        let true_weights: [f32; 16] = [0.0, 2.0, 0.0, 2.0, 
+                                       2.0, 0.0, 2.0, 0.0,
+                                       0.0, 2.0, 0.0, 2.0, 
+                                       2.0, 0.0, 2.0, 0.0];
+        let graph = BinaryGraph::from(data); // the fully connected graph
+ 
+        //let poly = Polynomial::new(vec![8.0, 0.0, 8.0, 0.0, 1.0]);
+        //let mut poly = [0.0 as f32; size_of::<f32>() * 8];
+        let mut weights: [f32; 4096] = [0.0; 4096];
+        weights[..16].copy_from_slice(&true_weights);
+        //poly[..5].copy_from_slice(&true_poly);
+        let weighted_polynomial = weighted_matching_polynomial_addresses(graph, &weights);
+        assert_eq!(weighted_polynomial[..5], [8.0, 0.0, 8.0, 0.0, 1.0]);
     }
 
-    // now take the product?
-    //let mut multi_prod = missing_nodes;
-    //let mut prod = iproduct!(non_zero_weights.clone(), non_zero_weights.clone())
-        //.filter(|((i, _),(j,_))| {
-            //let res = (i / graph_size, i % graph_size);
-            //let res2 = (j / graph_size, j % graph_size);
-            //// not same row
-            //// not same column
-            //// not column and row
-            //// not row and column
-            //res.0 != res2.0 && res.1 != res2.1 && res.1 != res2.0 && res.0 != res2.1
-        //});
-    //for weight in prod {
-        //println!("{:?}", weight);
-    //}        
-    
+    #[test]
+    fn test_weighted_matching_polynomial_from_addresses() {
+        let data = [
+            0b1101, 0b110, 0b11, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0,
+        ];
+        let true_weights: [f32; 16] = [0.0, 2.0, 0.0, 2.0, 
+                                       2.0, 0.0, 2.0, 0.0,
+                                       0.0, 2.0, 0.0, 2.0, 
+                                       2.0, 0.0, 2.0, 0.0];
+        let graph = BinaryGraph::from(data); // the fully connected graph
+ 
+        // get the addresses from the graph:
+        let (_, addresses) = calculate_matching_polynomial_pointer_addresses(graph);
+        
+        let mut weights: [f32; 4096] = [0.0; 4096];
+        weights[..16].copy_from_slice(&true_weights);
+        let weighted_polynomial = weighted_matching_polynomial_from_addresses(addresses, &weights, graph.initial_graph_size());
+        assert_eq!(weighted_polynomial[..5], [8.0, 0.0, 8.0, 0.0, 1.0]);
+    }
 
-    // we now have the indices of the non-zero weights
-    weighted_coefficient
-    
 }
